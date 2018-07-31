f45a5d10fde23e5497f9fca458a7251f050c9e6a
nnhw <nnhw@nnhw-pc.OLVS>
2018-07-31 12:44:52 +0300

    Сложение векторов

--- boid.py
+++ boid.py
@@ -23,6 +23,7 @@ class boid:
         self._vector_to_target = [0, 0]
         self._cohesion_vector = [0, 0]
         self._alignment_vector = [0, 0]
+        self._repulsion_vector = [0, 0]
         self._avoidance_vector = [0, 0]
 
         self._swarm = swarm
@@ -75,33 +76,34 @@ class boid:
 
     def _avoid_obstacles(self, obstacle, distance, x, y):
         # TODO: Implement "smart" avoidance algorithm
-        self._avoidance_vector = [1, 1]]
+        self._avoidance_vector = [
+            self._direction[0] + 0.1, self._direction[1] + 0.1]
 
     def _calculate_distances_in_swarm(self):
-        difference= []
+        difference = []
         for boid in self._swarm.boid:
-            coordinates, direction= boid.get_visual_info()
+            coordinates, direction = boid.get_visual_info()
             difference.append(abs(
                 self._coordinate[0] - coordinates[0]) + abs(self._coordinate[1] - coordinates[1]))
         return list(enumerate(difference))
 
     def _range_difference(self):
-        difference= self._calculate_distances_in_swarm()
+        difference = self._calculate_distances_in_swarm()
         for i in range(len(difference)):
             for j in range(len(difference)):
                 if difference[i][1] > difference[j][1]:
-                    temp= difference[i]
-                    difference[i]= difference[j]
-                    difference[j]= temp
+                    temp = difference[i]
+                    difference[i] = difference[j]
+                    difference[j] = temp
         return difference
 
     def _find_nearest_boids(self):
-        difference= self._range_difference()
-        self._buddy_1_coordinate, self._buddy_1_direction= self._swarm.boid[difference[self._swarm._number_of_boids - 2][0]].get_visual_info(
+        difference = self._range_difference()
+        self._buddy_1_coordinate, self._buddy_1_direction = self._swarm.boid[difference[self._swarm._number_of_boids - 2][0]].get_visual_info(
         )
-        self._buddy_2_coordinate, self._buddy_2_direction=self._swarm.boid[difference[self._swarm._number_of_boids - 3][0]].get_visual_info(
+        self._buddy_2_coordinate, self._buddy_2_direction = self._swarm.boid[difference[self._swarm._number_of_boids - 3][0]].get_visual_info(
         )
-        self._buddy_3_coordinate, self._buddy_3_direction=self._swarm.boid[difference[self._swarm._number_of_boids - 4][0]].get_visual_info(
+        self._buddy_3_coordinate, self._buddy_3_direction = self._swarm.boid[difference[self._swarm._number_of_boids - 4][0]].get_visual_info(
         )
 
     def _check_buddy_position(self, _coordinates):
@@ -111,48 +113,56 @@ class boid:
             return False
 
     def _add_random_noise_to_direction(self):
-        self._direction=0.99 * self._direction[0] +
-            random.choice((1, -1))*0.01*(random.random() * 2 * mt.pi)
+        self._direction[0] = 0.99 * self._direction[0] + \
+            random.choice((1, -1))*0.01*(random.random())
+        self._direction[1] = 0.99 * self._direction[1] + \
+            random.choice((1, -1))*0.01*(random.random())
 
     def _set_direction_to_point(self, _setpoint, _threshold):
-        location_error_x=_setpoint[0] - self._coordinate[0]
-        location_error_y=_setpoint[1] - self._coordinate[1]
-        hippo=mt.sqrt(mt.pow(location_error_x, 2) +
+        location_error_x = _setpoint[0] - self._coordinate[0]
+        location_error_y = _setpoint[1] - self._coordinate[1]
+        if location_error_x > _threshold and location_error_y > _threshold:
+            k = 1
+        else:
+            k = 0
+        hippo = mt.sqrt(mt.pow(location_error_x, 2) +
                         mt.pow(location_error_y, 2))
-        sin=(location_error_y / hippo)
-        cos=(location_error_x / hippo)
-        return cos, sin
+        sin = (location_error_y / hippo)
+        cos = (location_error_x / hippo)
+        return cos, sin, k
 
     def _cohesion(self):
-        central_point_x=(
+        central_point_x = (
             (self._buddy_1_coordinate[0]+self._buddy_2_coordinate[0]+self._buddy_3_coordinate[0])/3)
         central_point_y = (
             (self._buddy_1_coordinate[1]+self._buddy_2_coordinate[1]+self._buddy_3_coordinate[1])/3)
         central_point = [central_point_x, central_point_y]
-        self._cohesion_vector = self._set_direction_to_point(central_point, 10)
+        self._cohesion_vector[0], self._cohesion_vector[1], k = self._set_direction_to_point(
+            central_point, 10)
+        return k
 
     def _alignment(self):
         self._alignment_vector = [(self._buddy_1_direction[0] + self._buddy_2_direction[0] + self._buddy_3_direction[0])/3,
                                   (self._buddy_1_direction[1] + self._buddy_2_direction[1] + self._buddy_3_direction[1])/3]
 
+    def _repulsion(self):
+        self._repulsion_vector = [0, 0]
+
     def update_status(self):
         self._find_nearest_boids()
         obstacle = True
-        self._vector_to_target = self._set_direction_to_point(
-            boid._point_of_interest, 0)
+        self._vector_to_target[0], self._vector_to_target[1], k1 = self._set_direction_to_point(
+            boid._point_of_interest, 10)
         self._alignment()
-        self._cohesion()
+        k2 = self._cohesion()
 
         while obstacle is True:
             obstacle, distance, x, y = self._execute_vision()
             if obstacle is True:
                 self._avoid_obstacles(obstacle, distance, x, y)
 
-        # k1 = k2 = k3 = k4 = 1
-        # self._direction = (k1 * self._vector_to_target + k2 * self._alignment_vector + k3 * self._cohesion_vector + k4 * self._avoidance_vector)/2
+        self._direction = [(k1*self._vector_to_target[0] + k2*self._cohesion_vector[0] + 0.3 * self._alignment_vector[0] + self._avoidance_vector[0])/(1 + k1 + k2),
+                           (k1*self._vector_to_target[1] + k2*self._cohesion_vector[1] + 0.3 * self._alignment_vector[1] + self._avoidance_vector[1])/(1 + k1 + k2)]
 
-        self._direction = [(self._vector_to_target[0] + self._cohesion_vector[0] + self._alignment_vector[0])/3,
-                           (self._vector_to_target[1] + self._cohesion_vector[1] + self._alignment_vector[1])/3]
-        # self._direction = self._alignment_vector
-        # self._add_random_noise_to_direction()
+        self._add_random_noise_to_direction()
         self._move()
